5f6a461a01e6bea3e071a4862305b96fab4403cb
Maël <contact@maeln.com>
2018-05-14 13:57:05 +0200

    Command buffer.

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,9 @@ use vulkano::instance::PhysicalDevice;
 
 use vulkano::buffer::BufferUsage;
 use vulkano::buffer::CpuAccessibleBuffer;
+use vulkano::command_buffer::AutoCommandBufferBuilder;
+use vulkano::command_buffer::CommandBuffer;
+use vulkano::sync::GpuFuture;
 
 fn main() {
     let instance = Instance::new(None, &InstanceExtensions::none(), None)
@@ -51,6 +54,30 @@ fn main() {
         *content *= 2;
         println!("val: {}", *content);
     }
+
+    let source_content = 0..64;
+    let source = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), source_content)
+        .expect("Failed to create source buffer.");
+
+    let dest_content = (0..64).map(|_| 0);
+    let dest = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), dest_content)
+        .expect("Failed to create destination buffer.");
+
+    let command_buffer = AutoCommandBufferBuilder::new(device.clone(), queue.family())
+        .unwrap()
+        .copy_buffer(source.clone(), dest.clone())
+        .unwrap()
+        .build()
+        .unwrap();
+    let finished = command_buffer.execute(queue.clone()).unwrap();
+    finished
+        .then_signal_fence_and_flush()
+        .unwrap()
+        .wait(None)
+        .unwrap();
+    let src_content = source.read().unwrap();
+    let dest_content = dest.read().unwrap();
+    assert_eq!(&*src_content, &*dest_content);
 
     println!("Hello, world!");
 }
